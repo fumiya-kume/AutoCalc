d1484d852c3abf1169aa91966409390e644e711a
Fumiya Kume <fumiya.kume@hotmail.com>
2017-04-04 13:20:38 +0900

    Add Test and iOS Release Build Settiing

---
+++ AutoCalc.UnitTest/CalcTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AutoCalc.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoCalc.UnitTest
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            Assert.AreEqual(CalcModel.Add(1, 2), 3);
+            Assert.AreEqual(CalcModel.Add(2, 3), 5);
+        }
+    }
+}
---
+++ AutoCalc/AutoCalc/Model/CalcModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCalc.Model
+{
+    public class CalcModel
+    {
+        public static int Add(int i1, int i2)
+        {
+            return i1 + i2;
+        }
+    }
+}
